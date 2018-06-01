d2eaa9a07f6036d653e3b4334f1f8526404c45fb
Dimitri <Dimitri16h@gmail.com>
2018-06-01 14:11:45 -0400

    Winning logic added

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 //HTML variables
-var numPlayersOptions = document.getElementById("numPlayersOptions")
+var numPlayersOptions = document.getElementById("numPlayersOptions");
+var htmlTiles = document.getElementsByClassName("tile");
+var htmlBoard = document.getElementById("board");
 
 //Logic Variables
 var playerSign = ["X", "O"];
 var currentPlayer = 0;
+var board = [];
+var gameIsRunning = false;
 
 document.onreadystatechange = function() {
 	if (document.readyState == "interactive") {
@@ -32,20 +36,63 @@ function tileLogic(evt) {
 	console.log("You selected a tile! "+ tileNumber);
 	if (evt.target.innerHTML == "&nbsp;") {
 		evt.target.innerHTML = playerSign[currentPlayer];
+		checkForWin();
 		if (currentPlayer == 0) {
-			currentPlayer = 1
+			currentPlayer = 1;
 		} else {currentPlayer = 0;}
 	}
 }
 
-// function chooseNumOfPlayers() {
-// 	numPlayersOptions.style.display = 'block';
+function checkForWin() {
 
-// }
+	for (var i=0; i<htmlTiles.length; i++){
+		board[i] = htmlTiles[i].innerHTML;
+	}
 
-function gameLoop(){
+
+	//check all 3 horizontals for a win
+	if (board[0] === board[1] && board[0]===board[2] && board[0] != "&nbsp;") {
+		alert("We have a winner");
+	}
+	else if (board[3] === board[4] && board[3]===board[5] && board[3] != "&nbsp;") {
+		alert("We have a winner");
+	}
+	else if (board[6] === board[7] && board[6]===board[8] && board[6] != "&nbsp;") {
+		alert("We have a winner");
+	}
+
+
+	//check all 3 verticals for a win
+	else if (board[0] === board[3] && board[0]===board[6] && board[0] != "&nbsp;") {
+		alert("We have a winner");
+	}
+	else if (board[1] === board[4] && board[1]===board[7] && board[1] != "&nbsp;") {
+		alert("We have a winner");
+	}
+	else if (board[2] === board[5] && board[2]===board[8] && board[2] != "&nbsp;") {
+		alert("We have a winner");
+	}
+
+
+	//check the diagonals for a win
+	else if (board[0] === board[4] && board[0]===board[8] && board[0] != "&nbsp;") {
+		alert("We have a winner");
+	}
+	else if (board[2] === board[4] && board[2]===board[6] && board[2] != "&nbsp;") {
+		alert("We have a winner");
+	}
+}
+
+function startGame(){
+	gameIsRunning = true;
+ 	htmlBoard.style.display = "block";
+}
+
+function startOptions(){
 
 }
-function main() {
+
+function chooseNumOfPlayers() {
+	numPlayersOptions.style.display = 'block';
 
 }
