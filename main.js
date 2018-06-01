2c5adfc02406d6c6663fc7443951049964e5e487
Dimitri <Dimitri16h@gmail.com>
2018-06-01 11:52:45 -0400

    Added tile logic

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+//HTML variables
+var numPlayersOptions = document.getElementById("numPlayersOptions")
+
+//Logic Variables
+var playerSign = ["X", "O"];
+var currentPlayer = 0;
 
 document.onreadystatechange = function() {
 	if (document.readyState == "interactive") {
@@ -17,9 +23,29 @@ document.onreadystatechange = function() {
 function optionsLogic(evt) {
 	var option = evt.target.id;
 	console.log("You selected an option! " + option);
+	numPlayersOptions.style.display = 'none';
+
 }
 
 function tileLogic(evt) {
 	var tileNumber = evt.target.id;
 	console.log("You selected a tile! "+ tileNumber);
+	if (evt.target.innerHTML == "&nbsp;") {
+		evt.target.innerHTML = playerSign[currentPlayer];
+		if (currentPlayer == 0) {
+			currentPlayer = 1
+		} else {currentPlayer = 0;}
+	}
+}
+
+// function chooseNumOfPlayers() {
+// 	numPlayersOptions.style.display = 'block';
+
+// }
+
+function gameLoop(){
+
+}
+function main() {
+
 }
